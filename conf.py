1db70be0743ce347fbf5363bea04af1decd55681
Lex Li <support@lextm.com>
2020-01-13 10:16:08 -0500

    Disabled edit_on_github.py.

--- conf.py
+++ conf.py
@@ -31,8 +31,7 @@ sys.path.insert(0, os.path.abspath('_ext'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.intersphinx',
-    'edit_on_github'
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -385,10 +384,6 @@ feed_skip_regex = '(.)*index'
 feed_base_url = 'https://docs.obfuscar.com/'
 feed_description = 'Obfuscar Documentation'
 feed_author = 'Ryan Williams, Calvin Rien, Lex Li, RemObjects Software, and other contributors'
-
-if not on_rtd:
-    edit_on_github_project = 'obfuscar/docs'
-    edit_on_github_branch = 'master'
 
 def setup(app):
     on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
